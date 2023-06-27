579a5551d681f883dc974789474494d2f2249c8d
WTTomi <103531873+WonkeTomi@users.noreply.github.com>
2023-06-27 13:25:49 +0200

    initial commit

---
+++ src/test/java/com/askme/AskMe/AskMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.askme.AskMe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AskMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
