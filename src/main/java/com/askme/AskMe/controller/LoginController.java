3b2e35420bbb75a47acb619bfea43adf89a61728
WTTomi <103531873+WonkeTomi@users.noreply.github.com>
2023-06-29 13:42:38 +0200

    add login controller WIP

---
+++ src/main/java/com/askme/AskMe/controller/LoginController.java
@@ -0,0 +1,16 @@
+package com.askme.AskMe.controller;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginController {
+
+//    @PostMapping("/login")
+//    public ResponseEntity<AuthenticationRequest> login(@RequestBody AuthenticationRequest){
+//
+//    }
+}
