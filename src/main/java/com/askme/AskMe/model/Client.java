e3b081173359f990725b6346ff604fe2abcf9dfb
WTTomi <103531873+WonkeTomi@users.noreply.github.com>
2023-07-24 11:30:00 +0200

    add client implements Userdetails

--- src/main/java/com/askme/AskMe/model/Client.java
+++ src/main/java/com/askme/AskMe/model/Client.java
@@ -4,13 +4,17 @@ package com.askme.AskMe.model;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
 
 
 @Getter
 @Setter
 @Entity
 @Table
-public class Client {
+public class Client implements UserDetails {
 
     @Column
     @Id
@@ -31,4 +35,35 @@ public class Client {
     @Column
     @Enumerated(EnumType.STRING)
     private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+
 }
