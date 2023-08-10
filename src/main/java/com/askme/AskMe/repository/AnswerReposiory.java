c3d48d62bcc6118ed90d3ec8fa5ed820e3e22612
WTTomi <103531873+WonkeTomi@users.noreply.github.com>
2023-08-10 13:41:14 +0200

    add answer repository

---
+++ src/main/java/com/askme/AskMe/repository/AnswerReposiory.java
@@ -0,0 +1,8 @@
+package com.askme.AskMe.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AnswerReposiory extends JpaRepository {
+}
