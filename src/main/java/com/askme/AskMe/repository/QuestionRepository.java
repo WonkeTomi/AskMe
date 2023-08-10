384fe1905f157e1cc95c59cdd2ed71426952f96e
WTTomi <103531873+WonkeTomi@users.noreply.github.com>
2023-08-10 13:40:20 +0200

    add question repository

---
+++ src/main/java/com/askme/AskMe/repository/QuestionRepository.java
@@ -0,0 +1,9 @@
+package com.askme.AskMe.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface QuestionRepository extends JpaRepository {
+
+}
