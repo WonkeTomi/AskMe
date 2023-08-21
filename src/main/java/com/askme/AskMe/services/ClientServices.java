c400100d606cdf24cda30d7d43e5fb4dc68ad08c
WTTomi <103531873+WonkeTomi@users.noreply.github.com>
2023-08-21 11:57:36 +0200

    add question to client method to client service

--- src/main/java/com/askme/AskMe/services/ClientServices.java
+++ src/main/java/com/askme/AskMe/services/ClientServices.java
@@ -1,9 +1,13 @@
 package com.askme.AskMe.services;
 
 import com.askme.AskMe.model.Client;
+import com.askme.AskMe.model.Question;
 import com.askme.AskMe.repository.ClientRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Set;
 
 
 @Service
@@ -13,7 +17,18 @@ public class ClientServices {
     private final ClientRepository clientRepository;
 
     public Client getClientByUsername(String username) {
-        return clientRepository.findByUsername(username).orElse(null);
+        return clientRepository.findByUsername(username).orElseThrow(EntityNotFoundException::new);
+    }
+
+    public Client getClientById(Long id) {
+        return clientRepository.findById(id).orElseThrow(EntityNotFoundException::new);
+    }
+
+    public void addQuestionToClientList(Long clinetId, Question question) {
+        Client client = getClientById(clinetId);
+        Set<Question> clientQuestions = client.getQuestions();
+        clientQuestions.add(question);
+        clientRepository.save(client);
     }
 
 }
