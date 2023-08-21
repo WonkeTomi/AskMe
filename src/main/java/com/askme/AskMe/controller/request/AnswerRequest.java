63978f074beccd532d1156237f01d333828c1081
WTTomi <103531873+WonkeTomi@users.noreply.github.com>
2023-08-21 14:50:04 +0200

    add answer request

--- src/main/java/com/askme/AskMe/controller/request/AnswerRequest.java
+++ src/main/java/com/askme/AskMe/controller/request/AnswerRequest.java
@@ -1,4 +1,17 @@
 package com.askme.AskMe.controller.request;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
+@AllArgsConstructor
 public class AnswerRequest {
+
+    public Long questionId;
+    public String answer;
+    LocalDate date;
 }
